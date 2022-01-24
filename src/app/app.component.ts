b6c4bd547b9f4d048c610dd79576f95c94092898
Nagibator1337-xakep <MamkuIbalAzaza1337>
2022-01-24 20:05:45 +0500

    Refactoring w/ Angular functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,68 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = '';
   members: string[] = [];
+  errorMsg = '';
+  numberOfTeams = 0;
+  teams: string[][] = [[]];
+
+  color:string='beige';   // List element highlight color
 
   //Checking if user is trying to enter empty string or the same string consecutively
   addMember() {
-    if (this.newMemberName === '') {
-      this.errorTxt('Please enter the name', 3000);
-    } else if (this.members[this.members.length - 1] === this.newMemberName) {
-      this.errorTxt(
-        "This is the name you've just entered. <br/>Please enter different name",
-        4000
+    if (!this.newMemberName) {
+      //this.errorTxt('Please enter the name', 3000);
+      this.errorMsg='Please enter the name';
+    } else if (this.members.includes(this.newMemberName)) {
+      //this.errorTxt('Name already exists. <br>Please enter different name',4000);
+      this.errorMsg='Name already exists. Please enter different name';
+    } else {
+        this.members.push(this.newMemberName);
+        this.newMemberName = '';
+        this.errorMsg = '';
+
+      /* Changed for Angular *ngFor syntax
+
+      // Create a <li> node
+      let node = document.createElement('LI');
+      // Create a text node
+      let textnode = document.createTextNode(
+        this.members[this.members.length - 1]
       );
-    } else this.members.push(this.newMemberName);
-    console.log(this.members);
+      // Append the text to <li>
+      node.appendChild(textnode);
+      // Append <li> to <ul> with class="list"
+      document.getElementsByClassName('list')[0].appendChild(node);
+      */
+
+    }
   }
 
-  onKey(event: any) {
-    this.newMemberName = event.target.value;
+  onInput(member: string) {
+    this.newMemberName = member;
   }
 
   // Displaying Error message 'errorText' inside <div class="error-msg"></div> element for 'delay' milliseconds
+  // Changed for errorMsg flag
+  /*
   errorTxt(errorText: string, delay: number) {
     document.getElementsByClassName('error-msg')[0].innerHTML = errorText;
     setTimeout(function () {
       document.getElementsByClassName('error-msg')[0].innerHTML = '';
     }, delay);
+  } */
+
+  
+//TODO
+  splitIntoTeams(numberOfTeams:number):string[][] {
+    const random = (min:number, max:number) => Math.floor(Math.random() * (max - min)) + min;
+
+    let teams: string[][] = [[]];
+    let tempMembers = this.members;
+
+
+    tempMembers[random(0,tempMembers.length)]
+
+    return teams;
   }
+
 }
---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private li: ElementRef) { }
+
+  @Input() appHighlight='';
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.appHighlight);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.li.nativeElement.style.backgroundColor = color;
+  }
+  
+}
