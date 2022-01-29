42cc739bb29c33233010e4a8b637874d423d0f32
Nagibator1337-xakep <MamkuIbalAzaza1337>
2022-01-29 23:50:45 +0500

    Finished. Closes #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,67 +9,81 @@ export class AppComponent {
   newMemberName = '';
   members: string[] = [];
   errorMsg = '';
-  numberOfTeams = 0;
-  teams: string[][] = [[]];
+  numberOfTeams: number | '' = '';
+
+  teams: string[][] = [];
 
   color:string='beige';   // List element highlight color
 
   //Checking if user is trying to enter empty string or the same string consecutively
   addMember() {
     if (!this.newMemberName) {
-      //this.errorTxt('Please enter the name', 3000);
       this.errorMsg='Please enter the name';
     } else if (this.members.includes(this.newMemberName)) {
-      //this.errorTxt('Name already exists. <br>Please enter different name',4000);
       this.errorMsg='Name already exists. Please enter different name';
     } else {
         this.members.push(this.newMemberName);
         this.newMemberName = '';
         this.errorMsg = '';
-
-      /* Changed for Angular *ngFor syntax
-
-      // Create a <li> node
-      let node = document.createElement('LI');
-      // Create a text node
-      let textnode = document.createTextNode(
-        this.members[this.members.length - 1]
-      );
-      // Append the text to <li>
-      node.appendChild(textnode);
-      // Append <li> to <ul> with class="list"
-      document.getElementsByClassName('list')[0].appendChild(node);
-      */
-
     }
+  }
+
+  clear() {
+    this.members = [];
+    this.teams = [];
+    this.errorMsg = '';
+    this.newMemberName = '';
+    this.numberOfTeams = '';
   }
 
   onInput(member: string) {
     this.newMemberName = member;
   }
 
-  // Displaying Error message 'errorText' inside <div class="error-msg"></div> element for 'delay' milliseconds
-  // Changed for errorMsg flag
-  /*
-  errorTxt(errorText: string, delay: number) {
-    document.getElementsByClassName('error-msg')[0].innerHTML = errorText;
-    setTimeout(function () {
-      document.getElementsByClassName('error-msg')[0].innerHTML = '';
-    }, delay);
-  } */
-
-  
-//TODO
-  splitIntoTeams(numberOfTeams:number):string[][] {
-    const random = (min:number, max:number) => Math.floor(Math.random() * (max - min)) + min;
-
-    let teams: string[][] = [[]];
-    let tempMembers = this.members;
-
-
-    tempMembers[random(0,tempMembers.length)]
-
-    return teams;
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
   }
+
+  generateTeams() {
+    if (!this.numberOfTeams || this.numberOfTeams <=0) {
+      this.errorMsg='Please enter valid number of teams';
+      return;
+    }
+
+    if (this.numberOfTeams > this.members.length) { this.numberOfTeams = this.members.length }
+
+    this.teams = [[]];
+    const allMembers = [...this.members];
+
+    while (allMembers.length>0) {
+      for (let i = 0; i<this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex,1)[0];
+  
+        if (member == undefined) {
+          this.errorMsg = '';
+          break;
+        }
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }        
+      }
+    }
+    console.log(this.teams);
+
+  }
+
+  // // Functon for capitalizing first letter in every word of a string
+  // capitalize(name:string) : string {
+  //   let separateWord = name.split(' ');
+  //   for (let i = 0; i < separateWord.length; i++) {
+  //      separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+  //      separateWord[i].substring(1);
+  //   }
+  //   return separateWord.join(' ');
+  // }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
+    HighlightDirective,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
